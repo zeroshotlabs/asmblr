a3fa907212609d6f5ad652a9a32b802ab2994d65
praxis <praxis@7701b502-9f5b-4702-8660-5d38c0671700>
2014-01-27 22:48:06 +0000

    asmblr site architecture/load refactor
    git-svn-id: https://svn.stackware.com/Stackware/asmblr@5935 7701b502-9f5b-4702-8660-5d38c0671700

--- DOC_ROOT/index.php
+++ DOC_ROOT/index.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @file asmsrv.php asmblr boot file.
+ * @file index.php asmblr instance boot file.
  * @author Stackware, LLC
  * @version 4.0
  * @copyright Copyright (c) 2012-2014 Stackware, LLC. All Rights Reserved.
@@ -8,12 +8,12 @@
  * @copyright See COPYRIGHT.txt and LICENSE.txt.
  */
 
+
 // Optional
 define('START_TIME',microtime(TRUE));
 
-
 /**
- * The absolute path to asmblr's loader.
+ * The path to asmblr's loader.
  *
  * CONFIG: You should use an absolute path in production.
  *
@@ -25,307 +25,53 @@ require('../core/Load.inc');
 
 
 /**
- * Instance specific configuration.
+ * Define known hostnames and their apps.
  *
- * Remember, these are instance-wide configuration settings that will be applied
- * to ALL apps running under the instance.
+ * Always use forward slashes in the directory.
+ *
+ * A Load.inc file is assumed to be in AppRoot to load and execute the application.
+ *
+ * The hostname will be used as the base hostname which can be overridden by BaseURL
+ * when forming URLs.
+ *
+ * The pageset-name will be the default routing pageset for requests to that domain.  The
+ * default routing pageset and it's linker will be available as ps and lp, respectively.
+ *
+ * Exact hostname matching is performed first, then ordered matching is performed,
+ * least specific to most specific, which match domains prefixed with a period.
  */
-class Instance extends \asm\Instance
-{
-    /**
-     * Define known hostnames and their apps.
-     *
-     * Must be of the form hostname => array('/absolute/app/directory/','pageset-name')
-     *
-     * Always use forward slashes in the directory.
-     *
-     * The hostname will be used as the base hostname which can be overridden by BaseURL
-     * when forming URLs.
-     *
-     * The pageset-name will be the default routing pageset for requests to that domain.  The
-     * default routing pageset and it's linkset will be available as ps and lp, respectively.
-     *
-     * Exact hostname matching is performed first, then ordered matching is performed,
-     * least specific to most specific, which match domains prefixed with a period.
-     *
-     * @note This type of configuration really belongs in an App's file, not as a part of asmblr itself.
-     * @note This also means we need to rethink how DOC_ROOT is handled and this index.php file itself.
-     */
-    protected $Apps = array('mc.centz'=>array('/var/www/asmblr-apps/MC','mc'),
-                            'mcm.centz'=>array('/var/www/asmblr-apps/MC','mcm'));
-    /*
-     protected $Apps = array('asmblr-mc.appspot.com'=>'../../MC',
-             'localhost'=>'C:/Users/Hans Zaunere/Google Drive/asmblr-MC/MC',
-             'mc.wisp'=>'C:/Users/Hans Zaunere/Google Drive/asmblr-MC/MC',
-             'mcm.wisp'=>'C:/Users/Hans Zaunere/Google Drive/asmblr-apps/MC');
-    */
+$InstanceApps = array
+(
+    'mc.centz'=>array('AppRoot'=>'/var/www/asmblr-apps/MC',
+                    'RoutingPS'=>'mc',
+                'CacheManifest'=>FALSE,
+                     'CacheApp'=>FALSE),
 
-    /**
-     * Set to TRUE to cache app manifests from Google Docs.
-     * Setting to FALSE will disable and delete an existing cache file.
-     *
-     * NOTE: This should be TRUE only when manipulating the manifest, even during development.
-     */
-    protected $CacheManifest = TRUE;
+    'mcm.centz'=>array('AppRoot'=>'/var/www/asmblr-apps/MC',
+                     'RoutingPS'=>'mcm',
+                 'CacheManifest'=>FALSE,
+                      'CacheApp'=>FALSE),
 
+    'swcom.centz'=>array('AppRoot'=>'/var/www/asmblr-apps/www.stackware.com',
+                       'RoutingPS'=>'sw',
+                   'CacheManifest'=>FALSE,
+                        'CacheApp'=>FALSE)
+);
 
-    /**
-     * Set to TRUE to cache apps built from disk.
-     * Setting to FALSE will disable and delete an existing cache file.
-     */
-    protected $CacheApp = FALSE;
-}
 
 
 /**
- * Application specific configuration and execution.
+ * Begin application match and execution.
  */
-class App extends \asm\App
+$Request = \asm\Request::Init();
+$RequestHostname = \asm\Hostname::ToString($Request['Hostname']);
+
+if( isset($InstanceApps[$RequestHostname]) )
 {
-    // for full control of execution, extend/override this method
-    public function __construct( $Request,$Manifest )
-    {
-        parent::__construct($Request,$Manifest);
+    $InstanceApps[$RequestHostname]['Hostname'] = $RequestHostname;
+    $App = $InstanceApps[$RequestHostname];
 
-        // start with applying general purpose config settings
-        $C = $this->Config;
-
-        if( empty($C['Status']) || $C['Status'] !== 'Active' )
-            HTTP::_400();
-
-        // set a base directory
-        if( !empty($C['open_basedir']) )
-            $this->OpenBaseDir($C['open_basedir']);
-
-        // Activate the Framewire error handler
-        if( empty($C['error_handler']) )
-            set_error_handler(array($this,'ErrorHandler'));
-        else
-            set_error_handler(array($this,$C['error_handler']));
-
-        // Calculate app-wide URLs, modifying our Request array.  This sets
-        // $SiteURL and $MatchPath which are used for creating URLs (LinkSet) and
-        // matching pages (PageSet), respectively.
-        Request::CalcURLs($this->Request,$C['BaseURL']);
-
-        // instantiate system objects
-        // this sets dynamic properties - know your names
-
-        // we support more than one pageset though there is always a single default
-        // routing pageset which is tied to our requested domain (configured above in Instance)
-        // the default routing pageset and it's linker are available as ps and lp respetively
-        // other pagesets/linkers can be instantiated manually if needed
-        // a pageset must be defined
-        if( empty($Manifest['DefaultPageSet']) || empty($this->Pages[$Manifest['DefaultPageSet']]) )
-            throw new Exception("Unable to determine default pageset for '{$Manifest['Config']['Hostname']}'.");
-
-        $this->ps = new \asm\PageSet($this->Pages[$Manifest['DefaultPageSet']],$this->PageMaps[$Manifest['DefaultPageSet']]);
-        $this->lp = new \asm\LinkPage($this->ps,$this,$this->Request['SiteURL']);
-
-        $this->html = new \asm\enUSHTMLSet($this);
-
-        // links for managed images (via cnvyr)
-        $this->limg = new \asm\Linkcnvyr(\asm\URL::ToString($this->Request['SiteURL']).'/cnvyr/');
-
-        // general purpose key/value store
-        $this->page = new \asm\KeyValueSet;
-
-        // session based UI messages/alerts
-        $this->msg = new \asm\Messager;
-
-        // track form/request validation using an 'has-error' CSS class
-        $this->vr = new \asm\ValidationReport('has-error');
-
-        // templates should have access to some of these so Connect() them in
-        // if you later Connect() something using the same name it will overwrite the object here
-        $this->html->Connect(array('lp'=>$this->lp,'limg'=>$this->limg,'page'=>$this->page,'msg'=>$this->msg,'vr'=>$this->vr));
-
-
-        // uncomment to demonstrate mongo connectivity
-        /*
-        \asm\Inc::Ext('Mongo.inc');
-        $mongo = new \asm\Mongo;
-        $this->mydb = $mongo->Alias('mydb','mydb');
-        */
-
-
-        // if we're not caching the manifest we know we're not in production, so implement
-        // some easy toggling of debugging pages and templates and tweak our links to persist
-        if( empty($Manifest['CacheManifest']) || !empty($_GET['or']) )
-        {
-            if( !empty($_GET['debug']) )
-            {
-                $this->lp->SetBaseURL($this->Request['SiteURL'],$_GET);
-
-                $this->ps->DebugOn();
-                $this->html->DebugOn();
-
-                if( isset($this->mydb) )
-                    $this->mydb->DebugOn();
-            }
-        }
-
-
-        // match pages against the MatchPath to determine our executing page
-        // this logic is left verbose for easy customization
-
-        $OrderedMatch = $ExactMatch = NULL;
-
-        // first attempt a hierarchal ordered match/execute - most general to most specific URL path
-        if( $this->Request['MatchPath']['IsRoot'] === FALSE )
-        {
-            foreach( \asm\Path::Order($this->Request['MatchPath']) as $V )
-            {
-                if( ($OrderedMatch = $this->ps->Match($V)) !== NULL )
-                    break;
-            }
-        }
-
-        // if an ordered match isn't found, attempt an exact match.  if the ordered match is
-        // Weak, an exact match is also allowed
-        // i.e.  default:  /admin/ matches then a page with /admin/something will NOT match
-        //          Weak:  /admin/ matches then a page with /admin/something WILL match
-        if( $OrderedMatch === NULL || (strpos($OrderedMatch['Status'],'Weak') !== FALSE) )
-            $ExactMatch = $this->ps->Match(\asm\Path::ToString($this->Request['MatchPath']));
-/*
-        // determine which page(s) to execute
-        if( $OrderedMatch !== NULL )
-            $ExecPage = $OrderedMatch;
-        else if( $ExactMatch !== NULL )
-            $ExecPage = $ExactMatch;
-        else
-            $ExecPage = NULL;
-*/
-        // if no pages, it's a 404
-        if( empty($OrderedMatch) && empty($ExactMatch) )
-            $this->NoPageHandler();
-
-        // or if one isn't active
-        if( (!empty($OrderedMatch['Status']) && (strpos($OrderedMatch['Status'],'Active') === FALSE))
-         || (!empty($ExactMatch['Status']) && ($ExactMatch['Status'] !== 'Active')) )
-            $this->NoPageHandler();
-
-        $this->OrderedMatch = $OrderedMatch;
-        $this->ExactMatch = $ExactMatch;
-
-        // asmblr supports CLI execution natively, though some config directives
-        // only make sense when serving an HTTP request, or for certain pages
-        if( $this->Request['IsCLI'] === FALSE )
-        {
-            // IsBaseHostname indicates whether the request is using the full hostname
-            // configured by BaseURL.  If not, redirect to it.
-            if( $C['ForceBaseHostname'] === TRUE && $this->Request['IsBaseHostname'] === FALSE )
-            {
-                $this->Request['Hostname'] = $this->Request['BaseURL']['Hostname'];
-                HTTP::Location($this->Request);
-            }
-
-            // start a session if appropriate
-            if( !empty($C['StartSession']) && (strpos($C['PageNoSession'],$OrderedMatch['Name']) === FALSE) && (strpos($C['PageNoSession'],$ExactMatch['Name']) === FALSE))
-                session_start();
-
-            // seems to cause encoding errors, like for IE when the request doesn't finish normally
-            // and sometimes with Apache, though rarely - only when we var_dump early in execution it seems
-//             if( !empty($C['zlib_output']) )
-//                 ini_set('zlib.output_compression',TRUE);
-
-            if( !empty($C['mb_http_output']) )
-                mb_http_output($C['mb_http_output']);
-
-            // these can also be handled when creating the enUSHTMLSet template object
-            if( empty($C['ContentType']) )
-                $C['ContentType'] = 'text/html; charset=UTF-8';
-
-            header("Content-Type: {$C['ContentType']}");
-        }
-
-        // we're ready to execute the page and honor any app-wide directives or function
-        // note that this could be reordered if needed
-        foreach( $this->Directives as $V )
-        {
-            if( empty($this->{$V[0]}) )
-                throw new Exception("Directive object {$V[0]}' doesn't exist while executing app for '{$this->Config['Hostname']}'.");
-            else
-                $this->{$V[0]}->ApplyDirective($V[1],$V[2]);
-        }
-
-        // optionally execute a function just prior to the page and rendering
-        // if this function returns FALSE things are left to the function and nothing further is done
-        if( !empty($C['PreFunction']) )
-        {
-            if( $C['PreFunction']($this) !== FALSE )
-            {
-                // now execute the actual page(s)
-                if( !empty($OrderedMatch) )
-                    $this->ps->Execute($OrderedMatch);
-
-                if( !empty($ExactMatch) )
-                    $this->ps->Execute($ExactMatch);
-
-                // and finally begin rendering at the Base.tpl template
-                $this->html->Base();
-            }
-        }
-        // otherwise just execute the page and render
-        else
-        {
-            // now execute the actual page(s)
-            if( !empty($OrderedMatch) )
-                $this->ps->Execute($OrderedMatch);
-
-            if( !empty($ExactMatch) )
-                $this->ps->Execute($ExactMatch);
-
-            // and finally begin rendering at the Base.tpl template
-            $this->html->Base();
-        }
-    }
-
-
-    /**
-     * Handle a Page not found (404) error.
-     *
-     * Execution stops at this function.
-     */
-    public function NoPageHandler()
-    {
-        // Send the appropriate HTTP header.
-        \asm\HTTP::_404();
-
-        // Ensure we have a $html around and swap in the 404 template.
-        if( isset($this->html) )
-        {
-            $this->html->ReMap('Layout','Error404');
-
-            // Start rendering here since we don't return from this method.
-            $this->html->Base();
-        }
-
-        // Kill execution.
-        exit;
-    }
-}
-
-
-try
-{
-    $Request = \asm\Request::Init();
-
-    $inst = new Instance;
-
-    $Manifest = $inst->Match($Request);
-
-    // this does it all though could be multiple functions
-    $app = new App($Request,$Manifest);
-}
-catch( \Exception $E )
-{
-    if( isset($app) )
-        $app->html->Error500();
-
-    trigger_error($E->getMessage());
-    trigger_error($E->getTraceAsString());
-
-    \asm\HTTP::_500();
+    // hand control to our app's loader which is expected to carry on
+    include($App['AppRoot'].'/Load.inc');
 }
 
